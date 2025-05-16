a3345cbe1aaba82cbb1534b8f52ecaa904d9c5c7
Fahmi <fahmimiftah.faridz@gmail.com>
2025-05-16 18:44:47 +0700

    build: setup react vite tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-blue-600 p-4">
+      Tailwind berhasil! 🚀
+    </div>
+  );
+}
+
+export default App
